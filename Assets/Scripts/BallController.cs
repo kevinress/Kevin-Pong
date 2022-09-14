0bbe1d693e1ab54e4d04f46a8f53f75550ca63a2
kevinress <96115057+kevinress@users.noreply.github.com>
2022-09-14 23:26:08 +0700

    Adding UI & Game Manager
    Adding UI & Game Manager

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public Vector2 speed;
+    public Vector2 resetPosition;
     Rigidbody2D rig;
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
+    }
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
     }
 }
